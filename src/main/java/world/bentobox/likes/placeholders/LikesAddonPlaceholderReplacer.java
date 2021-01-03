0c47c5fb09cc168e77885ab70dd82a30f5db26f7
BONNe <bonne@bonne.id.lv>
2021-01-03 16:19:28 +0200

    Fixes #16
    Implement Visited Island placeholders.

--- src/main/java/world/bentobox/likes/placeholders/LikesAddonPlaceholderReplacer.java
+++ src/main/java/world/bentobox/likes/placeholders/LikesAddonPlaceholderReplacer.java
@@ -1,22 +1,30 @@
+///
+// Created by BONNe
+// Copyright - 2021
+///
+
 package world.bentobox.likes.placeholders;
 
+
 import org.eclipse.jdt.annotation.NonNull;
-import org.eclipse.jdt.annotation.Nullable;
 
 import world.bentobox.bentobox.api.addons.GameModeAddon;
-import world.bentobox.bentobox.database.objects.Island;
+import world.bentobox.bentobox.api.user.User;
 import world.bentobox.likes.LikesAddon;
 
 
+/**
+ * The interface Likes addon placeholder replacer.
+ */
 public interface LikesAddonPlaceholderReplacer
 {
 
     /**
      * @param addon the LikesAddon that registered the placeholder, cannot be null.
      * @param gameModeAddon the GameModeAddon that registered the placeholder, cannot be null.
-     * @param island the Island of the User, can be null.
+     * @param user the User, cannot be null.
      * @return the String containing the requested value or an empty String.
      */
     @NonNull
-    String onReplace(@NonNull LikesAddon addon, @NonNull GameModeAddon gameModeAddon, @Nullable Island island);
+    String onReplace(@NonNull LikesAddon addon, @NonNull GameModeAddon gameModeAddon, @NonNull User user);
 }
