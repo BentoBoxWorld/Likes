31fb298259884b9f213cc8c2b87a331649ba18e4
BONNe <bonne@bonne.id.lv>
2021-12-20 01:48:41 +0200

    Add annotations to the pladdon class.
    These annotations will allow Spigot to detect a plugin.

--- src/main/java/world/bentobox/likes/LikesPladdon.java
+++ src/main/java/world/bentobox/likes/LikesPladdon.java
@@ -8,6 +8,9 @@ import world.bentobox.bentobox.api.addons.Pladdon;
  *
  * @author YellowZaki
  */
+@Plugin(name="Pladdon", version="1.0")
+@ApiVersion(ApiVersion.Target.v1_16)
+@Dependency(value = "BentoBox")
 public class LikesPladdon extends Pladdon {
 
     @Override
